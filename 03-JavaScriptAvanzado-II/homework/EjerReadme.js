d063fc365f7edbc3e3c788a8fedefbbfc3e287d1
Cristian Molina <cristian84molina@gmail.com>
2022-11-03 22:24:41 -0300

    cambios 3/11

---
+++ 03-JavaScriptAvanzado-II/homework/EjerReadme.js
@@ -0,0 +1,15 @@
+function Repeatify (str, num) {
+    if ( num > 0) {
+    var hola = str.repeat(num);
+    console.log (hola)
+    }else if ( num < 0) {
+        console.log ('Error Numero Invalido')
+    }else {
+        console.log ('string vacio')
+    }
+}
+
+Repeatify('hola' ,5);
+
+
+
